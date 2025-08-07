604cd7a89ba8b904f64d31bf0b708891428aa4e0
harshada pol <pharshada300@gmail.com>
2025-08-07 12:52:05 +0530

    Add files via upload

---
+++ vijay.js
@@ -0,0 +1,7 @@
+function button1(){
+    var button1=document.getElementById('button1')
+    button1.style="background:blue;color:white;"
+    alert("form is successfully submitted")
+    confirm("confirm")
+    console.log("read more")
+}
